c5edb7573a69bb81ba629d36197247451cb10219
jehon <jehontan@gmail.com>
2022-11-23 14:57:28 -0500

    Use singularity container to unzip.

--- data/download_data.sh
+++ data/download_data.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 
-# Install 7z
-echo "Checking for 7z (might require installation)..."
-
-if sudo apt-get install p7zip-full -y; then
-    echo "7z found/installed"
-else
-    echo "Failed: Please install 7z (https://www.7-zip.org/7z.html)"
-    exit 1
-fi
+# Uses Singularity container to unzip.
 
 # Download, Unzip, and Remove zip
 if [ "$1" = "json" ]
@@ -16,7 +8,7 @@ then
 
     echo "Downloading JSONs ..."
     wget https://ai2-vision-alfred.s3-us-west-2.amazonaws.com/json_2.1.0.7z
-    7z x json_2.1.0.7z -y && rm json_2.1.0.7z
+    singularity run docker://crazymax/7zip 7za x json_2.1.0.7z -y && rm json_2.1.0.7z
     echo "saved folder: json_2.1.0"
 
 elif [ "$1" = "json_feat" ]
@@ -24,7 +16,7 @@ then
 
     echo "Downloading JSONs and Resnet18 feats ..."
     wget https://ai2-vision-alfred.s3-us-west-2.amazonaws.com/json_feat_2.1.0.7z
-    7z x json_feat_2.1.0.7z -y && rm json_feat_2.1.0.7z
+    singularity run docker://crazymax/7zip 7za x json_feat_2.1.0.7z -y && rm json_feat_2.1.0.7z
     echo "saved folder: json_feat_2.1.0"
 
 elif [ "$1" = "full" ]
@@ -32,7 +24,7 @@ then
 
     echo "Downloading JSONs, Resnet18 feats, Images, Videos, and PDDL states ..."
     wget https://ai2-vision-alfred.s3-us-west-2.amazonaws.com/full_2.1.0.7z
-    7z x full_2.1.0.7z -y && rm full_2.1.0.7z
+    singularity run docker://crazymax/7zip 7za x full_2.1.0.7z -y && rm full_2.1.0.7z
     echo "saved folder: full_2.1.0"
 
 else
